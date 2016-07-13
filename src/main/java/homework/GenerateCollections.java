b2b46ae0ac6d00f9d0ee80e1e723907d20194e29
james <Joyukr@ukr.net>
2016-07-13 17:32:01 +0300

    Generics Collections with StreamTokenizer

--- src/main/java/homework/GenerateCollections.java
+++ src/main/java/homework/GenerateCollections.java
@@ -1,26 +1,37 @@
 package homework;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.function.Supplier;
 
 public class GenerateCollections {
 
     public static void main(String[] args) {
 
+        File file = new File("/home/james/workspace/Itea/src/main/java/homework/someText.txt");
         Collection<String> collection = new ArrayList<>();
 
-        collection = generate(collection, () -> "name");
-
+        selectText(collection,file);
         collection.forEach(System.out::println);
+    }
+
+    private static void selectText(Collection<String> collection, File file) {
+        try {
+            StreamTokenizer tokenizer = new StreamTokenizer(new BufferedReader(new FileReader(file)));
+            while (tokenizer.nextToken() != StreamTokenizer.TT_EOF) {
+                if (tokenizer.ttype == StreamTokenizer.TT_WORD) {
+                    collection = generate(collection, () -> tokenizer.sval);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static Collection<String> generate(Collection<String> collection, Supplier<String> supplier) {
 
         collection.add(supplier.get());
-
         return collection;
-
     }
 }
