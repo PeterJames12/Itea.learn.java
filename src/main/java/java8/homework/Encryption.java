1fa43868669912ed21005016538811b5f70b6647
james <Joyukr@ukr.net>
2016-07-18 16:32:06 +0300

    Generate numbers

---
+++ src/main/java/java8/homework/Encryption.java
@@ -0,0 +1,49 @@
+package java8.homework;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Encryption {
+
+    public static void main(String[] args) {
+
+        giveMeSomeNumber(112, 123);
+
+        giveMeSomeString();
+    }
+
+    private static void giveMeSomeString() {
+
+        List<String> someString = new LinkedList<>();
+        someString.add("Hello i'm going to install linux linux linux");
+    }
+
+    private static void giveMeSomeNumber(Integer from, Integer to) {
+
+        List<Integer> generate = generate(from, to);
+
+        String result = generate.stream().filter(s -> s > 0).map(Character::toChars).map(Encryption::myToString).collect(Collectors.joining());
+        System.out.println(result);
+    }
+
+    private static String myToString(char[] chars) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char ch : chars) {
+            stringBuilder.append(ch);
+        }
+        return stringBuilder.toString();
+    }
+
+    private static List<Integer> generate(Integer from, Integer to) {
+
+        List<Integer> generate = new LinkedList<>();
+
+        for (int i = from; i < to; i++) {
+            generate.add(i);
+        }
+        return generate;
+    }
+}
